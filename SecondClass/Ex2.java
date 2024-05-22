77c12c5bb02166b9a4c4393d0a86ecdeec6b7279
joaovtfor <joaovtfor@hotmail.com>
2024-05-22 19:37:56 -0300

    `Add` - SecondClass

--- SecondClass/Ex2.java
+++ SecondClass/Ex2.java
@@ -1,10 +1,14 @@
 package SecondClass;
 import SecondClass.Carro;
+import SecondClass.Moto;
 
 public class Ex2 {
     public static void main(String[] args) {
         Carro carro01 = new Carro();
         Carro carro02 = new Carro();
+
+        Moto moto01 = new Moto();
+        Moto moto02 = new Moto();
 
         carro01.modelo = "Fusca";
         carro01.ano = 1989;
@@ -17,5 +21,17 @@ public class Ex2 {
 
         System.out.println("\nCarro 2: ");
         carro02.exibir();
+
+        moto01.modelo = "Ninja";
+        moto01.ano = 2010;
+
+        System.out.println("Moto 1: ");
+        moto01.exibir();
+        
+        moto01.modelo = "Ninja";
+        moto01.ano = 2010;
+
+        System.out.println("Moto 1: ");
+        moto01.exibir();
     }
 }
---
+++ SecondClass/Moto.java
@@ -0,0 +1,11 @@
+package SecondClass;
+
+public class Moto {
+    String modelo;
+    int ano;
+
+    void exibir() {
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+}
