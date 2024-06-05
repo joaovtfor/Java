cebe12c0459e3552ae273358d71236ca7d4f4c1c
joaovtfor <joaovtfor@hotmail.com>
2024-06-05 15:01:09 -0300

    `Add` - Unfinished homework

---
+++ FourthClass/CadastroVeiculo.java
@@ -0,0 +1,21 @@
+package FourthClass;
+
+class Veiculo {
+    private String modelo;
+    private int ano;
+    private String cor;
+
+    public Veiculo (String modelo, int ano, String cor) {
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
+
+    public class Moto extends Veiculo {
+
+    }
+}
+
+public class CadastroVeiculo {
+
+}
